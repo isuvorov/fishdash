b85340812c34d9ff2a5606ac5e34ea95d8c57195
isuvorov <hi@isuvorov.com>
2025-10-29 14:16:46 +0300

    feat: stable build

--- src/uniqBy.ts
+++ src/uniqBy.ts
@@ -1,8 +1,17 @@
 import type { ObjectMapper } from './types';
 
-// export type Mapper<T> = (value?: T, key?: string, arr?: Array<T>) => any;
-
-export const uniqBy = <T>(arr: Array<T>, mapper: ObjectMapper<T>): Array<T> =>
-  [...new Set(arr.map(mapper))].map((x) => arr.find((y) => mapper(y) === x));
+export const uniqBy = <T>(arr: Array<T>, mapper: ObjectMapper<T>): Array<T> => {
+  const seen = new Set<any>();
+  const result: Array<T> = [];
+  for (const item of arr) {
+    const mapped = mapper(item);
+    if (!seen.has(mapped)) {
+      seen.add(mapped);
+      result.push(item);
+    }
+  }
+  return result;
+}
+// [...new Set(arr.map(mapper))].map((x) => arr.find((y) => mapper(y) === x));
 
 export default uniqBy;
